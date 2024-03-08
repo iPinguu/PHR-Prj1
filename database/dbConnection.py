a5a518d3d2199af29e6bdfac5306726ab4c47fc3
Franz Luiz <luizdsy@gmail.com>
2024-03-07 23:26:47 -0500

    Final commit, CRUD endpoints defined

--- database/dbConnection.py
+++ database/dbConnection.py
@@ -18,20 +18,17 @@ from pymongo import MongoClient
 
 def mongoConnection():
     """
-        Defines the connection parameters and returns the 
-        instantiated pymongo object to be used by the server
+        Establishes a connection with the mongoDB and returns a MongoClient Object
     """
 
-    mongoHost = "127.0.0.1"
+    mongoHost = "localhost"
     mongoPort = 27017
 
     client = MongoClient(mongoHost, mongoPort)
-    db = client['PHR']
-    collection = db['users']
 
     try:
         client.admin.command('ping')
         print("[SERVER]: Pinged your deployment. You successfully connected to MongoDB!")    
-        return collection
+        return client
     except Exception as e:
         print(f'[SERVER]: {e}')
